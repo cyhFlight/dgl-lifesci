38e6b042bbaee2d7a5a39e3f4f652da7701d367d
Zhuhui-node108 <zhuhui@nibs.ac.cn>
2021-03-14 14:49:10 +0800

    typo fix

--- python/dgllife/data/pdbbind.py
+++ python/dgllife/data/pdbbind.py
@@ -85,7 +85,7 @@ class PDBBind(object):
     def __init__(self, subset, pdb_version, load_binding_pocket=True, remove_coreset_from_refinedset=True, sanitize=False, 
                  calc_charges=False, remove_hs=False, use_conformation=True,
                  construct_graph_and_featurize=ACNN_graph_construction_and_featurization,
-                 zero_padding=True, num_processes=None, **kwargs):
+                 zero_padding=True, num_processes=None):
         self.task_names = ['-logKd/Ki']
         self.n_tasks = len(self.task_names)
         self._read_data_files(pdb_version, subset, load_binding_pocket,remove_coreset_from_refinedset)
@@ -161,16 +161,16 @@ class PDBBind(object):
             if pdb_version == 'v2015':
                 with open(extracted_data_path + '/v2015/INDEX_core_data.2013','r') as f:
                     for line in f:
-                        line = line.strip().split()
-                        if line[0] != "#" and line[0] in pdbs:
-                            pdbs.remove(line[0])
+                        fields = line.strip().split()
+                        if fields[0] != "#" and fields[0] in pdbs:
+                            pdbs.remove(fields[0])
 
             if pdb_version == 'v2007':
-                with open (extracted_data_path + '/2007/INDEX.2017.core.data','r') as f:
+                with open (extracted_data_path + '/2007/INDEX.2007.core.data','r') as f:
                     for line in f:
-                        line = line.strip().split()
-                        if line[0] != "#" and line[0] in pdbs:
-                            pdbs.remove(line[0])      
+                        fields = line.strip().split()
+                        if fields[0] != "#" and fields[0] in pdbs:
+                            pdbs.remove(fields[0])      
 
         self.ligand_files = [os.path.join(
             extracted_data_path, pdb_version, pdb, '{}_ligand.sdf'.format(pdb)) for pdb in pdbs]
